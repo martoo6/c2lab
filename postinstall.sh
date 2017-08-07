d4c5e925a296a4abde9af5e12f805817dda1fe2e
Martin Silberkasten <martoo6@gmail.com>
2017-08-07 00:23:55 -0300

    Removed jdk downlad

--- postinstall.sh
+++ postinstall.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 curl -Ls https://git.io/sbt > sbt && chmod 0755 sbt
 
-printenv
-
-echo "Checking if we are in openshift"
-if [ ! -z "$OPENSHIFT_DATA_DIR" ]; then
-    echo "Checking if $OPENSHIFT_DATA_DIR/jdk exists"
-    if [ ! -d "$OPENSHIFT_DATA_DIR/jdk" ]; then
-      wget -qO- --no-check-certificate --no-cookies --header 'Cookie: oraclelicense=accept-securebackup-cookie' 'http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-8u144-linux-x64.tar.gz' | tar -zxvf $OPENSHIFT_DATA_DIR && mv $OPENSHIFT_DATA_DIR/jdk1.8.0_144 $OPENSHIFT_DATA_DIR/jdk
-    fi
-fi
+#echo "Checking if we are in openshift"
+#if [ ! -z "$OPENSHIFT_DATA_DIR" ]; then
+#    echo "Checking if $OPENSHIFT_DATA_DIR/jdk exists"
+#    if [ ! -d "$OPENSHIFT_DATA_DIR/jdk" ]; then
+#      wget -qO- --no-check-certificate --no-cookies --header 'Cookie: oraclelicense=accept-securebackup-cookie' 'http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-8u144-linux-x64.tar.gz' | tar -zxvf $OPENSHIFT_DATA_DIR && mv $OPENSHIFT_DATA_DIR/jdk1.8.0_144 $OPENSHIFT_DATA_DIR/jdk
+#    fi
+#fi
 
 mkdir -p .sbt/launchers
 mkdir -p .sbt/0.13.16

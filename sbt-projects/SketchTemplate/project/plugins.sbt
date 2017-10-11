7d943d0a18528f5d855905c93eb7be0333d8e770
Martin Silberkasten <martoo6@gmail.com>
2017-10-11 07:28:14 -0300

    Updated cdlab library

--- sbt-projects/SketchTemplate/project/plugins.sbt
+++ sbt-projects/SketchTemplate/project/plugins.sbt
@@ -1,4 +1,4 @@
-addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.17")
+addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.20")
 
 addSbtPlugin("org.ensime" % "sbt-ensime" % "1.12.12")
 
--- sbt-projects/SketchTemplate/src/main/scala/ThreeJSApp.scala
+++ sbt-projects/SketchTemplate/src/main/scala/ThreeJSApp.scala
@@ -3,6 +3,7 @@ import main.recorder._
 
 import scala.collection.mutable
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
+import JSConverters._
 
 @JSExportTopLevel("ThreeJSApp")
 class ThreeJSApp extends BasicCanvas with DrawingUtils with SimplexNoise {

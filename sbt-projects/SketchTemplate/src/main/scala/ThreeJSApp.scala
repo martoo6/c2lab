4d8611dcd6ecc5f99faa320171e649c082d57a27
Martin Silberkasten <martoo6@gmail.com>
2017-10-19 08:19:44 -0300

    Removed jsConverters from skethes template

--- sbt-projects/SketchTemplate/src/main/scala/ThreeJSApp.scala
+++ sbt-projects/SketchTemplate/src/main/scala/ThreeJSApp.scala
@@ -3,7 +3,6 @@ import main.recorder._
 
 import scala.collection.mutable
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
-import JSConverters._
 
 @JSExportTopLevel("ThreeJSApp")
 class ThreeJSApp extends BasicCanvas with DrawingUtils with SimplexNoise {
